79b8af43493e1ac66e8e2aca8627d41a1fcdc9d5
misgana96 <misganayoseph@gmail.com>
2023-02-22 12:01:54 +0300

    add app bar component

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-
+import './assets/styles/styles.css'
 loadFonts()
 
 createApp(App)
